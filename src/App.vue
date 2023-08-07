3f75163afe2952482ee9a84de18a7cb3dc8d8a17
HIGOR J. S. CAMPOS <hh1217higorcampos@hotmail.com>
2023-08-07 17:23:13 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,16 @@
 </template>
 <script>
 
-  import { TEMA_VERMELHO } from '@/assets/javascript/globalConstants.js'
+    import { TEMA_VERMELHO } from '@/assets/javascript/globalConstants.js'
 
-  export default {
-    mounted(){
-        document.documentElement.style.setProperty('--cor-tema', TEMA_VERMELHO.mainColor);
-        document.documentElement.style.setProperty('--cor-hover', TEMA_VERMELHO.HoverColor);
-        document.documentElement.style.setProperty('--shadow-hover', TEMA_VERMELHO.shadowHover);
-        document.documentElement.style.setProperty('--shadow-input', TEMA_VERMELHO.shadowInput);
+    export default {
+        mounted(){
+            document.documentElement.style.setProperty('--cor-tema', TEMA_VERMELHO.mainColor);
+            document.documentElement.style.setProperty('--cor-hover', TEMA_VERMELHO.HoverColor);
+            document.documentElement.style.setProperty('--shadow-hover', TEMA_VERMELHO.shadowHover);
+            document.documentElement.style.setProperty('--shadow-input', TEMA_VERMELHO.shadowInput);
+        }
     }
-  }
 </script>
 
 
